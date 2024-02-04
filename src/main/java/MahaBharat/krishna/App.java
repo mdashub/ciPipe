36b232fdc9baf8f53eb669af91d3f32ddda9f7fb
MangalaDas <loveyouchopra95@gmail.com>
2024-02-04 17:29:25 +0530

    Testing CI pipline

---
+++ src/main/java/MahaBharat/krishna/App.java
@@ -0,0 +1,13 @@
+package MahaBharat.krishna;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/MahaBharat/krishna/TestClassDemo.java
@@ -0,0 +1,38 @@
+package MahaBharat.krishna;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class TestClassDemo {
+	public WebDriver driver;
+
+	@BeforeTest
+	public void setUp() {
+		WebDriverManager.firefoxdriver().setup();
+		 driver = new FirefoxDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void testApp() {
+		driver.get("https://www.google.com");
+		driver.getTitle();
+	}
+
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+		System.out.println("CLosed Test Cases");
+	}
+
+}
